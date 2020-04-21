79d5c52a22d7d2a22013fe3da1dd6d8f1aeb6e4f
A_Asaker <39923238+a-asaker@users.noreply.github.com>
2020-04-21 07:11:03 +0200

    Update py3.py

--- py3.py
+++ py3.py
@@ -8,7 +8,7 @@ if "win" in os.sys.platform :
  colorama.init()
 print()
 
-#Print Date & Time At The Top Of The Terminal
+#Print Date & Time At The Top
 #Not Important, Comment This Section If You Want!
 def terminal_time():
  while 1:
@@ -23,21 +23,23 @@ threading.Thread(target=terminal_time,daemon=True).start()
 cmnd=""
 while cmnd != 'exit':
  try:
-  cmnd=input("\033[0m\n{} \033[1;4;33mPy3\033[0m{}\n   \033[1;31m {}{}{}{}\033[1;37m ".format(chr(5295),chr(5292),chr(5307),chr(5933),chr(5933),chr(5582)))
-
-  try:
-   if "cd" in cmnd.strip().split():
-    s_cmnd=cmnd.strip().split()
-    path=s_cmnd[s_cmnd.index('cd')+1]
-    os.chdir(path)
-    cmnd=cmnd.replace("cd","")
-    cmnd=cmnd.replace(path,"")
-   print("\033[94m	 ",end="")
-   exec(cmnd)
-  except Exception as ex:
-   print("\033[0;96m\r",end="")
-   os.sys.stdout.flush()
-   st_code=os.system(cmnd)
-   print("\033[1;91;107m"+str(ex),end="") if st_code else ""
+  in_cmnd=input("\033[0m\n{} \033[1;4;33mPy3\033[0m{}\n   \033[1;31m {}{}{}{}\033[1;37m ".format(chr(5295),chr(5292),chr(5307),chr(5933),chr(5933),chr(5582)))
+  cmnds=in_cmnd.split("&&")
+  for cmnd in cmnds:
+   try:
+    cmnd=cmnd.strip()
+    if "cd" in cmnd.strip().split():
+     s_cmnd=cmnd.strip().split()
+     path=s_cmnd[s_cmnd.index('cd')+1]
+     os.chdir(path)
+     cmnd=cmnd.replace("cd","")
+     cmnd=cmnd.replace(path,"")
+    print("\033[94m      ",end="")
+    exec(cmnd)
+   except Exception as ex:
+    print("\033[0;96m\r",end="")
+    os.sys.stdout.flush()
+    st_code=os.system(cmnd)
+    print("\033[1;91;107m"+str(ex),end="") if st_code else ""
  except : break
 print("\033[0m")
